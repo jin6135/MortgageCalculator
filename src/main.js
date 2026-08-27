9a832ebc4b5bd4b5cf5325e9879918a67f952f3a
Jin Y <jinyou2009.12@gmail.com>
2026-08-27 10:43:24 -0400

    added preliminary calculations; insurance for under 20% down payment by the CHMC to be implemented soon

--- src/main.js
+++ src/main.js
@@ -1,15 +1,35 @@
 import './style.css'
 
-const monthlyMortgageAmt = document.getElementById('M');
-const principal = document.getElementById('P');
-const monthlyInterestRate = document.getElementById('r');
-const totalNumMonthlyPayments = document.getElementById('n');
+const form = document.getElementById("mortgage-form");
 
 const amortization = document.getElementById("amortization");
-const amortizationValue = document.getElementById("amortization-value");
+const homePrice = document.getElementById("home-price");
+const downPayment = document.getElementById("down-payment");
+const interestRate = document.getElementById("interest-rate");
+const paymentFrequency = document.getElementById("payment-frequency");
 
-amortization.addEventListener("input", function () {
-  amortizationValue.textContent = amortization.value;
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const price = Number(homePrice.value);
+  const down = Number(downPayment.value);
+  const annualRate = Number(interestRate.value);
+  const years = Number(amortization.value);
+  const frequency = Number(paymentFrequency.value);
+
+  /* calculations */
+  const principal = price - down;
+  const r = Math.pow(
+    1 + (annualRate / 100) / 2,
+    2 / frequency
+  ) - 1;
+  const n = years * frequency;
+
+  const M = principal * (r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1);
+
+  console.log("Principal:", principal);
+  console.log("r:", r);
+  console.log("n:", n);
+  console.log("Mortgage Payment:", M);
 });
 
-const amortizationYears = Number(amortization.value);
